ca56072b2d5c36c613adc912f790228e35b88317
shaminb128 <shaminb128@gmail.com>
2016-02-02 21:07:33 -0800

    Create Display.java

---
+++ Display.java
@@ -0,0 +1,20 @@
+package com.example.shamin.weatherforecast;
+
+/**
+ * Created by Shamin on 12/6/2015.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toolbar;
+
+public class Display extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+
+
+    }
+}
